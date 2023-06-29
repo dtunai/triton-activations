8aaa3b580f4e0455720b91343c7a5ffe79db465d
simuark <simuarc@gmail.com>
2023-06-29 19:04:50 +0300

    smol typo change wip branch

--- triton_activations/activations.py
+++ triton_activations/activations.py
@@ -1,6 +1,6 @@
 import torch
 
-from triton_activations import funcs
+from triton_activations import functions
 from typing import Any, Optional, Union
 
 def tanh_activation(x: torch.Tensor):
@@ -8,7 +8,7 @@ def tanh_activation(x: torch.Tensor):
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    funcs.tanh_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
+    functions.tanh_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
     return output
 
 def relu_activation(x: torch.Tensor):
@@ -16,7 +16,7 @@ def relu_activation(x: torch.Tensor):
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    relu_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
+    functions.relu_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
     return output
 
 
@@ -25,7 +25,7 @@ def softplus_activation(x: torch.Tensor):
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    funcs.softplus_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
+    functions.softplus_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
     return output
 
 def softsign_activation(x: torch.Tensor):
@@ -33,7 +33,7 @@ def softsign_activation(x: torch.Tensor):
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    funcs.softsign_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
+    functions.softsign_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
     return output
 
 def sigmoid_activation(x: torch.Tensor):
@@ -41,7 +41,7 @@ def sigmoid_activation(x: torch.Tensor):
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    funcs.sigmoid_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
+    functions.sigmoid_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
     return output
 
 def silu_activation(x: torch.Tensor):
@@ -49,7 +49,7 @@ def silu_activation(x: torch.Tensor):
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    funcs.silu_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
+    functions.silu_activation_kernel[grid](x, output, n_elements, BLOCK_SIZE=1024)
     return output
 
 
@@ -58,7 +58,7 @@ def gelu_activation(x: torch.Tensor, approximate: bool = True):
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    funcs.gelu_activation_kernel[grid](x, output, approximate, n_elements, BLOCK_SIZE=1024)
+    functions.gelu_activation_kernel[grid](x, output, approximate, n_elements, BLOCK_SIZE=1024)
     return output
 
 def softmax_activation(x: torch.Tensor, axis_ld: Optional[Union[int, tuple[int, ...]]] = -1):
@@ -68,5 +68,5 @@ def softmax_activation(x: torch.Tensor, axis_ld: Optional[Union[int, tuple[int, 
     assert x.is_cuda and output.is_cuda
     n_elements = output.numel()
     grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']),)
-    funcs.softmax_activation_kernel[grid](x, output, axis_ld, approximate, n_elements, BLOCK_SIZE=1024)
+    functions.softmax_activation_kernel[grid](x, output, axis_ld, approximate, n_elements, BLOCK_SIZE=1024)
     return output
